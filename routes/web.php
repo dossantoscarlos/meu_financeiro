9414cb4c3767a1ea1d8cea802dc02fd7f479cbf3
Carlos Eduardo <anjoazrael10@gmail.com>
2024-06-02 23:33:24 -0300

    test(auth): Removido tests de auth do livewire

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
-use App\Livewire\Cliente;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-Route::get('cliente', Cliente::class);
-
-
-require __DIR__.'/auth.php';
-
-
+Route::get('/', function () {
+    return redirect('/admin/register');
+});
