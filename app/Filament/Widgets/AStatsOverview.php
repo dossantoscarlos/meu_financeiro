d46335dbb6e782514a9c56e649870622af5d8265
Carlos Eduardo <anjoazrael10@gmail.com>
2024-07-10 18:43:32 -0300

    refactor: Removendo codigo duplicado

--- app/Filament/Widgets/AStatsOverview.php
+++ app/Filament/Widgets/AStatsOverview.php
@@ -12,6 +12,13 @@ use Illuminate\Support\Facades\Log;
 
 class AStatsOverview extends BaseWidget
 {
+
+    private function stat_item($data) : string
+    {
+        return 'R$ '.number_format(floatval($data), 2, ',', '.');
+    }
+
+
     protected function getStats(): array
     {
 
@@ -19,6 +26,7 @@ class AStatsOverview extends BaseWidget
         $renda = Renda::whereUserId($authId)->first();
         $date = Carbon::now();
 
+        $total = 0.0;
         $mes = ($date->month >= 1 && $date->month <= 9) ? strval('0'.$date->month) : $date->month;
         $ano = $date->year;
         $mesAno = "{$mes}/{$ano}";
@@ -28,31 +36,18 @@ class AStatsOverview extends BaseWidget
             ['mes_ano', '>=', $mesAno],
         ])->first()?->toArray() ?? [];
 
-        $stat = [
-            Stat::make('Renda Inicial', 'R$ '.number_format(floatval($renda->saldo), 2, ',', '.') ?? 'R$ 0,00'),
-            Stat::make('Custo previsto', 'R$ 0,00'),
-            Stat::make('Renda atual', 'R$ '.number_format(floatval($renda->saldo), 2, ',', '.') ?? 'R$ 0,00'),
-        ];
-
         if (empty($plano) || $plano['gastos'] == null) {
             $date = now();
             Log::info("{$date} - Array de plano esta vazio ", $plano);
-
-            return $stat;
-        }
-
-        if (! empty($renda)) {
-
+        }elseif (!empty($renda)) {
             $total = strval($plano['gastos']['valor']) ?? 0.0;
             Log::debug("total de gastos: {$total}");
-
-            return [
-                Stat::make('Renda inicial', 'R$ '.number_format(floatval($renda->saldo), 2, ',', '.')),
-                Stat::make('Custo previsto', 'R$ '.number_format(floatval($total), 2, ',', '.')),
-                Stat::make('Renda atual', 'R$ '.number_format(floatval($renda->custo ?? $renda->saldo), 2, ',', '.')),
-            ];
         }
 
-        return $stat;
+        return [
+            Stat::make('Renda Inicial', $this->stat_item($renda->saldo ?? 0)),
+            Stat::make('Custo previsto',$this->stat_item($total)),
+            Stat::make('Renda atual', $this->stat_item($renda->custo ?? $renda->saldo ?? 0)),
+        ];
     }
 }
