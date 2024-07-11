af165775e63ee14c1be32e9d8451a930989ad07d
Carlos Eduardo <anjoazrael10@gmail.com>
2024-07-11 01:07:29 -0300

    refactor:Removido verificacao de plano

--- app/Filament/Widgets/AStatsOverview.php
+++ app/Filament/Widgets/AStatsOverview.php
@@ -36,10 +36,7 @@ class AStatsOverview extends BaseWidget
             ['mes_ano', '>=', $mesAno],
         ])->first()?->toArray() ?? [];
 
-        if (empty($plano) || $plano['gastos'] == null) {
-            $date = now();
-            Log::info("{$date} - Array de plano esta vazio ", $plano);
-        }elseif (!empty($renda)) {
+        if (!empty($renda)) {
             $total = strval($plano['gastos']['valor']) ?? 0.0;
             Log::debug("total de gastos: {$total}");
         }
